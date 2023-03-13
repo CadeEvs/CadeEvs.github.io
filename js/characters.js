79ac6ed7464cb7a4423be321857779a316638cb6
Cade <cadegriffin101@gmail.com>
2023-03-13 09:30:51 -0400

    Added groups

--- js/characters.js
+++ js/characters.js
@@ -1,45 +1,55 @@
 var characters;
+var groups;
 var template;
 
-var oldClickedButton;
+var selectedCharacterButton;
+var selectedGroupButton;
 
 function onCharacterClicked(button) {
+  if (button == undefined) {
+    return;
+  }
+
   let character = characters[button.id];
 
+  // update attribute texts
   Object.entries(character).forEach(attribrute => {
     let attributeKey = attribrute[0]
 
     let text = document.getElementById(attributeKey + "-text");
-    
+    if (text == undefined) {
+      return;
+    }
+
     if (template[attributeKey] != undefined) {
       text.innerHTML = "<b>" + template[attributeKey] + "</b>" + attribrute[1];
     }
     else {
       text.innerHTML = attribrute[1];
     }
-    
   });
 
   // select and unselected old button if there is one
   button.classList.add("active");
 
-  if (oldClickedButton != null && oldClickedButton != button) {
-    oldClickedButton.classList.remove("active");
+  if (selectedCharacterButton != null && selectedCharacterButton != button) {
+    selectedCharacterButton.classList.remove("active");
   }
-  oldClickedButton = button;
+  selectedCharacterButton = button;
 }
 
-function onJsonFetched(json) {
-  characters = json.characters;
-  template = json.template;
-  console.log("Initializing...");
-
+function createCharacterButtons(selectedGroup, shouldForceClear) {
   let character_list = document.getElementById("character-list");
 
-  character_list.innerHTML = "";
+  if (shouldForceClear) {
+    character_list.innerHTML = "";
+  }
 
   Object.entries(characters).forEach(character => {
     if (character[1].is_debug == true) {
+      return;
+    }
+    if (selectedGroup != "All" && character[1].group != selectedGroup) {
       return;
     }
 
@@ -53,7 +63,55 @@ function onJsonFetched(json) {
     character_list.appendChild(button);
   });
 
+  // auto select first entry
   onCharacterClicked(character_list.children[0]);
+}
+
+function onGroupClicked(group) {
+  createCharacterButtons(group.id, true);
+
+  // select and unselected old button if there is one
+  group.classList.add("active");
+
+  if (selectedGroupButton != null && selectedGroupButton != group) {
+    selectedGroupButton.classList.remove("active");
+  }
+  selectedGroupButton = group;
+}
+
+function onJsonFetched(json) {
+  groups = json.groups
+  characters = json.characters;
+  template = json.template;
+
+  let group_list = document.getElementById("group-list");
+
+  // Create "All" group button
+  {
+    let button = document.createElement("button");
+    button.classList.add("list-group-item");
+    button.classList.add("list-group-item-action");
+    button.id = "All";
+    button.onclick = function () { onGroupClicked(button, false) };
+
+    button.innerHTML = "All";
+    group_list.appendChild(button);
+  }
+
+  // Create dynamic group buttons
+  groups.forEach(group => {
+    let button = document.createElement("button");
+    button.classList.add("list-group-item");
+    button.classList.add("list-group-item-action");
+    button.id = group;
+    button.onclick = function () { onGroupClicked(button, false) };
+
+    button.innerHTML = group;
+    group_list.appendChild(button);
+  });
+
+  // auto select first entry
+  onGroupClicked(group_list.children[0]);
 }
 
 function onWindowLoaded() {
