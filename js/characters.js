8aa5fcd1e8b79e049428eba5443b96793745762a
Cade <cadegriffin101@gmail.com>
2023-03-13 07:33:07 -0400

    Refactored attribute updating and added Known Connections

--- js/characters.js
+++ js/characters.js
@@ -1,22 +1,17 @@
 var characters;
+var template;
 
 var oldClickedButton;
 
 function onCharacterClicked(button) {
-  let name_text = document.getElementById("name-text");
-  let race_text = document.getElementById("race-text");
-  let age_text = document.getElementById("age-text");
-  let status_text = document.getElementById("status-text");
-  let description_text = document.getElementById("description-text");
-
   let character = characters[button.id];
 
-  name_text.innerHTML = character.name;
-  race_text.innerHTML = "Race: " + character.race;
-  age_text.innerHTML = "Age: " + character.age;
-  status_text.innerHTML = "Status: " + character.status;
-  description_text.innerHTML = character.description;
+  Object.entries(character).forEach(attribrute => {
+    let text = document.getElementById(attribrute[0] + "-text");
+    text.innerHTML = template[attribrute[0]] + attribrute[1];
+  });
 
+  // select and unselected old button if there is one
   button.classList.add("active");
 
   if (oldClickedButton != null && oldClickedButton != button) {
@@ -26,31 +21,32 @@ function onCharacterClicked(button) {
 }
 
 function onJsonFetched(json) {
-  characters = json;
+  characters = json.characters;
+  template = json.template;
   console.log("Initializing...");
 
   let character_list = document.getElementById("character-list");
 
   character_list.innerHTML = "";
 
-  Object.entries(json).forEach(character => {
+  Object.entries(characters).forEach(character => {
     let button = document.createElement("button");
     button.classList.add("list-group-item");
     button.classList.add("list-group-item-action");
     button.id = character[0];
-    button.onclick = function() {onCharacterClicked(button)};
+    button.onclick = function () { onCharacterClicked(button) };
 
     button.innerHTML = character[1].name;
     character_list.appendChild(button);
   });
-  
+
   onCharacterClicked(character_list.children[0]);
 }
 
 function onWindowLoaded() {
   fetch("https://raw.githubusercontent.com/CadeEvs/CadeEvs.github.io/main/data/characters.json")
-  .then((response) => response.json())
-  .then((data) => onJsonFetched(data.characters));
+    .then((response) => response.json())
+    .then((data) => onJsonFetched(data));
 }
 
 window.onload = onWindowLoaded;
