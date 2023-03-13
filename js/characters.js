a816d4ea20ceba087f418e78fe0bc5b2a3cab7c1
Cade <cadegriffin101@gmail.com>
2023-03-13 05:44:33 -0400

    Character generation and selection

--- js/characters.js
+++ js/characters.js
@@ -1,11 +1,43 @@
-function initialize(json) {
+var characters;
+
+function onCharacterClicked(button) {
+  let name_text = document.getElementById("name-text");
+  let race_text = document.getElementById("race-text");
+  let age_text = document.getElementById("age-text");
+  let description_text = document.getElementById("description-text");
+
+  let character = characters[button.id];
+
+  name_text.innerHTML = character.name;
+  race_text.innerHTML = "Race: " + character.race;
+  age_text.innerHTML = "Age: " + character.age;
+  description_text.innerHTML = character.description;
+}
+
+function onJsonFetched(json) {
+  characters = json;
   console.log("Initializing...");
 
-  Object.entries(json.characters).forEach(character => {
-    console.log(character[1].name)
+  let character_list = document.getElementById("character-list");
+
+  character_list.innerHTML = "";
+
+  Object.entries(json).forEach(character => {
+    let button = document.createElement("button");
+    button.classList.add("list-group-item");
+    button.classList.add("list-group-item-action");
+    button.id = character[0];
+    button.onclick = function() {onCharacterClicked(button)};
+
+    button.innerHTML = character[1].name;
+    character_list.appendChild(button);
   });
 }
 
-fetch("https://raw.githubusercontent.com/CadeEvs/CadeEvs.github.io/main/data/characters.json")
+function onWindowLoaded() {
+  fetch("https://raw.githubusercontent.com/CadeEvs/CadeEvs.github.io/main/data/characters.json")
   .then((response) => response.json())
-  .then((data) => initialize(data));
+  .then((data) => onJsonFetched(data.characters));
+}
+
+window.onload = onWindowLoaded;
