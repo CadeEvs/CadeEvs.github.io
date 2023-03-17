c63886d2321c9298edfd4e478de8d2f67fd761e3
Cade <cadegriffin101@gmail.com>
2023-03-17 04:01:14 -0400

    Updated locations page

--- js/locations.js
+++ js/locations.js
@@ -4,62 +4,65 @@ var template;
 var oldClickedButton;
 
 function onCharacterClicked(button) {
-  let location = locations[button.id];
+    let location = locations[button.id];
 
-  Object.entries(location).forEach(attribrute => {
-    let attributeKey = attribrute[0]
+    Object.entries(location).forEach(attribrute => {
+        let attributeKey = attribrute[0]
 
-    let text = document.getElementById(attributeKey + "-text");
-    
-    if (template[attributeKey] != undefined) {
-      text.innerHTML = "<b>" + template[attributeKey] + "</b>" + attribrute[1];
+        let text = document.getElementById(attributeKey + "-text");
+
+        if (template[attributeKey] != undefined) {
+            text.innerHTML = "<b>" + template[attributeKey] + "</b>" + attribrute[1];
+        } else {
+            text.innerHTML = attribrute[1];
+        }
+
+    });
+
+    // select and unselected old button if there is one
+    button.classList.add("active");
+
+    if (oldClickedButton != null && oldClickedButton != button) {
+        oldClickedButton.classList.remove("active");
     }
-    else {
-      text.innerHTML = attribrute[1];
-    }
-    
-  });
-
-  // select and unselected old button if there is one
-  button.classList.add("active");
-
-  if (oldClickedButton != null && oldClickedButton != button) {
-    oldClickedButton.classList.remove("active");
-  }
-  oldClickedButton = button;
+    oldClickedButton = button;
 }
 
 function onJsonFetched(json) {
-  locations = json.locations;
-  template = json.template;
-  console.log("Initializing locations...");
+    locations = json.locations;
+    template = json.template;
+    console.log("Initializing locations...");
 
-  let character_list = document.getElementById("character-list");
+    let character_list = document.getElementById("character-list");
 
-  character_list.innerHTML = "";
+    character_list.innerHTML = "";
 
-  Object.entries(locations).forEach(character => {
-    if (character[1].is_debug == true) {
-      return;
-    }
+    Object.entries(locations).forEach(character => {
+        if (character[1].is_debug == true) {
+            return;
+        }
 
-    let button = document.createElement("button");
-    button.classList.add("list-group-item");
-    button.classList.add("list-group-item-action");
-    button.id = character[0];
-    button.onclick = function () { onCharacterClicked(button) };
+        let button = document.createElement("button");
+        button.classList.add("list-group-item");
+        button.classList.add("list-group-item-action");
+        button.id = character[0];
+        button.onclick = function () {
+            onCharacterClicked(button)
+        };
 
-    button.innerHTML = character[1].name;
-    character_list.appendChild(button);
-  });
+        const title = character[1].poi.substring(0, character[1].poi.indexOf("."));
+        
+        button.innerHTML = "<div class=\"ms-2 me-auto\"><div class=\"fw-bold\">" + character[1].name + "</div>" + title + "</div>";
+        character_list.appendChild(button);
+    });
 
-  onCharacterClicked(character_list.children[0]);
+    onCharacterClicked(character_list.children[0]);
 }
 
 function onWindowLoaded() {
-  fetch("https://raw.githubusercontent.com/CadeEvs/CadeEvs.github.io/main/data/locations.json")
-    .then((response) => response.json())
-    .then((data) => onJsonFetched(data));
+    requestLocationData().then((json) => {
+        onJsonFetched(json);
+    });
 }
 
 window.addEventListener("load", onWindowLoaded);
